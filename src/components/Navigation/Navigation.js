20d848baf3b24854fb2dc9e643a62b2437926adb
Chribtoper <xcstolfax@gmail.com>
2022-01-12 17:07:58 -0700

    Created a placeholder Searchbar for Navigation called NavigationSearch

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,12 +4,13 @@ import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import AppBar from "@mui/material/AppBar";
 import { useNavigate } from "react-router-dom";
 import { connect } from "react-redux";
 import { getUserSelector } from "../../selectors/userSelectors";
 import { userActions } from "../../actions/userActions";
+import NavigationSearch from "./NavigationSearch";
 
 const Navigation = props => {
   const navigate = useNavigate();
@@ -24,36 +25,45 @@ const Navigation = props => {
   };
 
   return (
-      <AppBar>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Button onClick={handleOnClick} name="home" color="inherit">
-            Home
+    <AppBar>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{ display: { xs: "none", sm: "block" } }}
+        >
+          My Bookshelf
+        </Typography>
+        <NavigationSearch />
+        <Button onClick={handleOnClick} name="home" color="inherit">
+          Home
+        </Button>
+        {props.user.loggedIn ? (
+          <Button onClick={handleSignout} name="sign-out" color="inherit">
+            Sign Out
           </Button>
-          {props.user.loggedIn ? (
-            <Button onClick={handleSignout} name="sign-out" color="inherit">
-              Sign Out
+        ) : (
+          <>
+            <Button onClick={handleOnClick} name="sign-in" color="inherit">
+              Sign In
             </Button>
-          ) : (
-            <>
-              <Button onClick={handleOnClick} name="sign-in" color="inherit">
-                Sign In
-              </Button>
-              <Button onClick={handleOnClick} name="sign-up" color="inherit">
-                Sign Up
-              </Button>
-            </>
-          )}
-        </Toolbar>
-      </AppBar>
+            <Button onClick={handleOnClick} name="sign-up" color="inherit">
+              Sign Up
+            </Button>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
 
