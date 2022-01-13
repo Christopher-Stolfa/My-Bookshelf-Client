0c0827ee7eacf04572a8b717d88f51509c364104
Chribtoper <xcstolfax@gmail.com>
2022-01-13 15:53:06 -0700

    Added back sign in and sign up pages with the correct case sensitivity

---
+++ src/pages/SignUpPage/SignUpPage.js
@@ -0,0 +1,171 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { Link, useNavigate } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { getUserSelector } from "../../selectors/userSelectors";
+import { userActions } from "../../actions/userActions";
+import { connect } from "react-redux";
+import { routes } from "../../config";
+
+const theme = createTheme();
+
+const Copyright = props => (
+  <Typography variant="body2" color="text.secondary" align="center" {...props}>
+    {"Copyright © "}
+    <Link color="inherit" to={routes.home}>
+      My Bookshelf
+    </Link>{" "}
+    {new Date().getFullYear()}
+    {"."}
+  </Typography>
+);
+
+const SignUpPage = ({ user: { loggedIn }, signUp }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loggedIn) navigate(routes.home);
+    return () => {};
+  }, [loggedIn]);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const inputData = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      firstName: formData.get("firstName"),
+      lastName: formData.get("lastName"),
+      displayName: formData.get("displayName")
+    };
+    signUp(inputData);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="displayName"
+                  label="Display Name"
+                  name="displayName"
+                  autoComplete="display-name"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link to={routes.signIn} variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+SignUpPage.propTypes = {
+  signUp: PropTypes.func.isRequired,
+  user: PropTypes.shape({
+    message: PropTypes.string,
+    loggedIn: PropTypes.bool.isRequired,
+    userData: PropTypes.object
+  })
+};
+
+const mapStateToProps = state => ({
+  user: getUserSelector(state)
+});
+
+const actionCreators = {
+  signUp: userActions.signUp
+};
+
+export default connect(mapStateToProps, actionCreators)(SignUpPage);
