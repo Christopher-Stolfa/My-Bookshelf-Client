d7684674e856060aa7942eb73a5026d78367f369
Chribtoper <xcstolfax@gmail.com>
2022-01-13 16:56:25 -0700

    Added a rough draft of a SearchResultCard, SearchResultsPage renders many SearchResultCards with dividers

--- src/components/Cards/SearchResultCard.js
+++ src/components/Cards/SearchResultCard.js
@@ -1,1 +1,57 @@
 import React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+
+const SearchResultCard = ({ book }) => {
+  const theme = useTheme();
+
+  return (
+    <Card variant="none" sx={{ display: "flex" }}>
+      <CardMedia
+        component="img"
+        sx={{ width: 128, minWidth: 128, height: 168, minHeight: 168 }}
+        image={book.volumeInfo.imageLinks.thumbnail}
+        alt={book.volumeInfo.title}
+      />
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
+          <Typography component="div" variant="h5">
+            {book.volumeInfo.title}
+          </Typography>
+          {book.volumeInfo.authors.map((author) => (
+            <Typography
+              key={author}
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              {author}
+            </Typography>
+          ))}
+          <Typography
+            variant="subtitle2"
+            color="text.secondary"
+            component="div"
+            sx={{
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+          >
+            {book.volumeInfo.description}
+          </Typography>
+        </CardContent>
+        <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}></Box>
+      </Box>
+    </Card>
+  );
+};
+
+export default SearchResultCard;
--- src/pages/SearchResultsPage/SearchResultsPage.js
+++ src/pages/SearchResultsPage/SearchResultsPage.js
@@ -2,26 +2,51 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
+import Divider from "@mui/material/Divider";
+import Stack from "@mui/material/Stack";
 import { bookActions } from "../../actions/bookActions";
 import { getBookSelector } from "../../selectors/bookSelector";
+import SearchResultCard from "../../components/Cards/SearchResultCard";
 
-const SearchResultsPage = props => {
-  return <h1>Search Results Page</h1>;
+const SearchResultsPage = ({ books: { bookSearchData } }) => {
+  if (bookSearchData.length < 1) {
+    return <h1>No search results.</h1>;
+  } else {
+    return (
+      <>
+        <h1>Search results:</h1>
+        <Stack spacing={2}>
+          {bookSearchData.map((book) => (
+            <div key={`${book.id}-search-result-div`}>
+              <SearchResultCard
+                key={`${book.id}-search-result-card`}
+                book={book}
+              />
+              <Divider
+                sx={{ marginTop: "8px", marginBottom: "8px" }}
+                key={`${book.id}-divider`}
+              />
+            </div>
+          ))}
+        </Stack>
+      </>
+    );
+  }
 };
 
 SearchResultsPage.propTypes = {
   searchBook: PropTypes.func.isRequired,
   books: PropTypes.shape({
-    bookSearchData: PropTypes.array.isRequired
-  }).isRequired
+    bookSearchData: PropTypes.array.isRequired,
+  }).isRequired,
 };
 
-const mapStateToProps = state => ({
-  books: getBookSelector(state)
+const mapStateToProps = (state) => ({
+  books: getBookSelector(state),
 });
 
 const actionCreators = {
-  searchBook: bookActions.searchBook
+  searchBook: bookActions.searchBook,
 };
 
 export default connect(mapStateToProps, actionCreators)(SearchResultsPage);
