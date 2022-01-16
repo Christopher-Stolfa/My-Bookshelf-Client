1e3fe9e34d6602c489621b701cc79b963937a53b
Chribtoper <xcstolfax@gmail.com>
2022-01-15 18:15:17 -0700

    Removed success and error palette colors since the wrapper does not override the snackbar

--- src/themes/libraryTheme.js
+++ src/themes/libraryTheme.js
@@ -1,20 +1,13 @@
 import { createTheme } from "@mui/material/styles";
 
 const colors = {
-  skyBlue: "#6386C7",
-  cherryRed: "#EC1616"
+  skyBlue: "#6386C7"
 };
 
 export const libraryTheme = createTheme({
   palette: {
     primary: {
       main: colors.skyBlue
-    },
-    error: {
-      main: colors.cherryRed
-    },
-    success: {
-      main: "#9AB219"
     }
   }
 });
