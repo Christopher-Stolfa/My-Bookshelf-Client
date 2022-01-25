eabf66e66f95ca5cbe653372c830f63229ee09c7
Chribtoper <xcstolfax@gmail.com>
2022-01-25 00:38:53 -0700

    This page renders the search results components when its pathname is search-results, otherwise it loads an empty container that holds the outlet for then nested route component BookResultPage

--- src/pages/SearchResultsPage/SearchResultsPage.js
+++ src/pages/SearchResultsPage/SearchResultsPage.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 import Divider from "@mui/material/Divider";
 import Stack from "@mui/material/Stack";
@@ -12,56 +12,77 @@ import SearchResultCard from "../../components/Cards";
 import SearchResultLoading from "../../components/Loaders/SearchResultLoading";
 import { searchTypes } from "../../types/searchTypes";
 import { SearchResultPagination } from "../../components/Pagination";
+import { routes } from "../../config";
 
 const SearchResultsPage = ({
   searchResults: { bookSearchData },
   isLoading,
 }) => {
-  return (
-    <Container component="main">
-      {isLoading && (
-        <>
-          <h1>Loading...</h1>
-          <Stack spacing={2}>
-            {Array.from(
-              { length: 10 },
-              () => new Date().getTime() + Math.random()
-            ).map((key, i) => (
-              <div key={`${key}-${i}`}>
-                <SearchResultLoading key={`${key}-${i}-search-result-card`} />
-                <Divider
-                  sx={{ marginTop: "8px", marginBottom: "8px" }}
-                  key={`${key}-${i}-divider`}
-                />
-              </div>
-            ))}
-          </Stack>
-        </>
-      )}
-      {!isLoading && bookSearchData.length > 0 && (
-        <>
-          <h1>Search results:</h1>
-          <Stack spacing={2}>
-            {bookSearchData.map((book) => (
-              <div key={`${book.googleBooksId}-search-result-div`}>
-                <SearchResultCard
-                  key={`${book.googleBooksId}-search-result-card`}
-                  book={book}
-                />
-                <Divider
-                  sx={{ marginTop: "8px", marginBottom: "8px" }}
-                  key={`${book.googleBooksId}-divider`}
-                />
-              </div>
-            ))}
-          </Stack>
-          <SearchResultPagination />
-        </>
-      )}
-      {!isLoading && bookSearchData.length < 1 && <h1>No search results.</h1>}
-      <Outlet />
-    </Container>
-  );
+  const initialState = { isSelected: false, book: {} };
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [selectedBook, setSelectedBook] = useState(initialState);
+
+  const handleSelectBook = (book) => {
+    setSelectedBook({ isSelected: true, book });
+    navigate(`${routes.searchResults}/${book.googleBooksId}`);
+  };
+
+  if (location.pathname === routes.searchResults) {
+    return (
+      <Container component="main">
+        {isLoading && (
+          <>
+            <h1>Loading...</h1>
+            <Stack spacing={2}>
+              {Array.from(
+                { length: 10 },
+                () => new Date().getTime() + Math.random()
+              ).map((key, i) => (
+                <div key={`${key}-${i}`}>
+                  <SearchResultLoading key={`${key}-${i}-search-result-card`} />
+                  <Divider
+                    sx={{ marginTop: "8px", marginBottom: "8px" }}
+                    key={`${key}-${i}-divider`}
+                  />
+                </div>
+              ))}
+            </Stack>
+          </>
+        )}
+        {!isLoading && bookSearchData.length > 0 && (
+          <>
+            <h1>Search results:</h1>
+            <Stack spacing={2}>
+              {bookSearchData.map((book) => (
+                <div key={`${book.googleBooksId}-search-result-div`}>
+                  <SearchResultCard
+                    key={`${book.googleBooksId}-search-result-card`}
+                    book={book}
+                    handleSelectBook={handleSelectBook}
+                  />
+                  <Divider
+                    sx={{ marginTop: "8px", marginBottom: "8px" }}
+                    key={`${book.googleBooksId}-divider`}
+                  />
+                </div>
+              ))}
+            </Stack>
+            <SearchResultPagination />
+          </>
+        )}
+        {!isLoading && bookSearchData.length < 1 && <h1>No search results.</h1>}
+      </Container>
+    );
+  } else {
+    return (
+      <Container component="main">
+        <Outlet
+          context={[selectedBook, setSelectedBook, initialState]}
+        />
+      </Container>
+    );
+  }
 };
 
 SearchResultsPage.propTypes = {
