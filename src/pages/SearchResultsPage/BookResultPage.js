dd9844d763dadc602c7762d1d6f8d7c8f57caaff
Chribtoper <xcstolfax@gmail.com>
2022-01-25 00:39:57 -0700

    This component is aware of where it received its bookId, on unmount it cleans up setting the selectedBook state of its parent to its initial state

--- src/pages/SearchResultsPage/BookResultPage.js
+++ src/pages/SearchResultsPage/BookResultPage.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useOutletContext, useParams } from "react-router-dom";
 
 const BookResultPage = (props) => {
-  return <h1>Book Result Page</h1>;
+  const params = useParams();
+  const [selectedBook, setSelectedBook, initialState] = useOutletContext();
+
+  // When a user exists this page or backs out of it, reset SearchResultsPage selectedBook state back to its initial state.
+  useEffect(() => {
+    if (!selectedBook.isSelected) {
+      console.log(
+        "User accessed this page manually, we must make a fetch with the ID they entered and handle it from there."
+      );
+    } else {
+      console.log("User accessed this page through search results");
+    }
+    return () => {
+      setSelectedBook({ initialState });
+    };
+  }, []);
+  return <h1>{params.bookId}</h1>;
 };
 
 export default BookResultPage;
