06acb0ba9e0050a0b157d2b6bb8a26170a900777
Chribtoper <xcstolfax@gmail.com>
2022-01-26 15:42:22 -0700

    Added an action for searching a book by ID

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -15,11 +15,28 @@ const searchBook = (inputData) => async (dispatch) => {
     const { data } = await searchService.searchBook(inputData);
     dispatch(getSearchBookSuccess(data));
     setSnackbarSuccess(data, dispatch);
-  } catch (err) {
-    setSnackbarError(err.response.data, dispatch);
+  } catch (error) {
+    setSnackbarError(error.response.data, dispatch);
   } finally {
     dispatch(stopAction(searchTypes.GET_SEARCH_BOOK_FETCH));
   }
 };
 
-export const searchActions = { searchBook };
+const searchBookById = (inputData) => async (dispatch) => {
+  const getSearchBookByIdSuccess = (data) => ({
+    type: searchTypes.GET_SEARCH_BOOK_BY_ID_SUCCESS,
+    payload: data,
+  });
+  try {
+    dispatch(startAction(searchTypes.GET_SEARCH_BOOK_BY_ID_FETCH));
+    const { data } = await searchService.searchBook(inputData);
+    dispatch(getSearchBookByIdSuccess(data));
+    setSnackbarSuccess(data, dispatch);
+  } catch (error) {
+    setSnackbarError(error.response.data, dispatch);
+  } finally {
+    dispatch(stopAction(searchTypes.GET_SEARCH_BOOK_BY_ID_FETCH));
+  }
+};
+
+export const searchActions = { searchBook, searchBookById };
