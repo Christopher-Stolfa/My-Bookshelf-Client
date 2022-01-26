3f2d4aaccab2d1abd5ffd22d87ad06ecd328b165
Chribtoper <xcstolfax@gmail.com>
2022-01-26 15:44:49 -0700

    Set up redux related methods and prop types

--- src/pages/SearchResultsPage/BookResultPage.js
+++ src/pages/SearchResultsPage/BookResultPage.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { useOutletContext, useParams } from "react-router-dom";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
 
 const BookResultPage = (props) => {
   const params = useParams();
@@ -18,7 +20,22 @@ const BookResultPage = (props) => {
       setSelectedBook({ initialState });
     };
   }, []);
-  return <h1>{params.bookId}</h1>;
+
+  // TODO: Make it check if loading alongside isSelected
+  return (
+    <Container>
+      {!selectedBook.isSelected && (
+        <Typography component="div" variant="h3">
+          No book selected
+        </Typography>
+      )}
+      {selectedBook.isSelected && (
+        <Typography component="div" variant="h3">
+          Book ID: {params.bookId}
+        </Typography>
+      )}
+    </Container>
+  );
 };
 
 export default BookResultPage;
