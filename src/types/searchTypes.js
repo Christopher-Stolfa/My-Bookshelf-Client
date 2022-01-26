c168b77f9dc4d45f5ba5f291d1e9743fade3d11b
Chribtoper <xcstolfax@gmail.com>
2022-01-26 15:44:03 -0700

    Added search type for fetching book by id and success type

--- src/types/searchTypes.js
+++ src/types/searchTypes.js
@@ -4,7 +4,15 @@ const GET_SEARCH_BOOK_FETCH = "GET_SEARCH_BOOK_FETCH";
 // Book search receives data successfully
 const GET_SEARCH_BOOK_SUCCESS = "GET_SEARCH_BOOK_SUCCESS";
 
+// Fetch a specific book by ID
+const GET_SEARCH_BOOK_BY_ID_FETCH = "GET_SEARCH_BOOK_BY_ID_FETCH";
+
+// Book search by ID receives data successfully
+const GET_SEARCH_BOOK_BY_ID_SUCCESS = "GET_SEARCH_BOOK_BY_ID_SUCCESS";
+
 export const searchTypes = {
   GET_SEARCH_BOOK_FETCH,
   GET_SEARCH_BOOK_SUCCESS,
+  GET_SEARCH_BOOK_BY_ID_FETCH,
+  GET_SEARCH_BOOK_BY_ID_SUCCESS,
 };
