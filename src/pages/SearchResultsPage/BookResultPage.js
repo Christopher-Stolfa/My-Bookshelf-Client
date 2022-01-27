25e90c5d816ad6ec26f541a9ca7bf4d66735406c
Chribtoper <xcstolfax@gmail.com>
2022-01-26 22:59:09 -0700

    Removed context state to favor redux instead, this page checks if the data is there and if not it will search for the book by book id

--- src/pages/SearchResultsPage/BookResultPage.js
+++ src/pages/SearchResultsPage/BookResultPage.js
@@ -1,41 +1,70 @@
 import React, { useEffect } from "react";
-import { useOutletContext, useParams } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { searchTypes } from "../../types/searchTypes";
+import { searchActions } from "../../actions/searchActions";
+import { getSelectedBookSelector } from "../../selectors/searchSelector";
+import { useParams } from "react-router-dom";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import { checkIfLoading } from "../../selectors/uiSelectors";
 
-const BookResultPage = (props) => {
-  const params = useParams();
-  const [selectedBook, setSelectedBook, initialState] = useOutletContext();
+const BookResultPage = ({ isLoading, searchBookById, selectedBookData }) => {
+  const { bookId } = useParams();
 
+  // TODO: Better handling of if the ID does not exist
+  const isEmpty = (obj) => {
+    if (Object.keys(obj).length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
   // When a user exists this page or backs out of it, reset SearchResultsPage selectedBook state back to its initial state.
   useEffect(() => {
-    if (!selectedBook.isSelected) {
+    if (isEmpty(selectedBookData)) {
       console.log(
         "User accessed this page manually, we must make a fetch with the ID they entered and handle it from there."
       );
+      const inputData = { data: JSON.stringify({ googleBooksId: bookId }) };
+      searchBookById(inputData);
     } else {
       console.log("User accessed this page through search results");
     }
-    return () => {
-      setSelectedBook({ initialState });
-    };
+    return () => {};
   }, []);
 
-  // TODO: Make it check if loading alongside isSelected
+  useEffect(() => {
+    console.log(isLoading);
+  }, [isLoading]);
   return (
     <Container>
-      {!selectedBook.isSelected && (
-        <Typography component="div" variant="h3">
-          No book selected
+      {isLoading && isEmpty(selectedBookData) && (
+        <Typography component="div" variant="h5">
+          Loading...
         </Typography>
       )}
-      {selectedBook.isSelected && (
-        <Typography component="div" variant="h3">
-          Book ID: {params.bookId}
+      {!isLoading && !isEmpty(selectedBookData) && (
+        <Typography component="div" variant="h5">
+          Book ID: {bookId}
         </Typography>
       )}
     </Container>
   );
 };
 
-export default BookResultPage;
+BookResultPage.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  searchBookById: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  selectedBookData: getSelectedBookSelector(state),
+  isLoading: checkIfLoading(state, searchTypes.GET_SEARCH_BOOK_BY_ID_FETCH),
+});
+
+const actionCreators = {
+  searchBookById: searchActions.searchBookById,
+};
+
+export default connect(mapStateToProps, actionCreators)(BookResultPage);
