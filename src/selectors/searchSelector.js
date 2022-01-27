fd5db52ab7c5d4a70affdeeb95860be9a7c7ea17
Chribtoper <xcstolfax@gmail.com>
2022-01-26 23:00:21 -0700

    Created a reducer for accessing the nested searchResultData obj

--- src/selectors/searchSelector.js
+++ src/selectors/searchSelector.js
@@ -1,10 +1,15 @@
 import { createSelector } from "reselect";
 // selector
 const getSearchState = (state) => state.searchState.searchResults;
+const getSelectedBookState = (state) =>
+  state.searchState.searchResults.selectedBookData;
 // reselect function
 export const getSearchSelector = createSelector(
   [getSearchState],
   (searchResults) => searchResults
 );
 
-export default getSearchSelector;
+export const getSelectedBookSelector = createSelector(
+  [getSelectedBookState],
+  (selectedBookData) => selectedBookData
+);
