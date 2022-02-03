4eb92d4d5928ddc6f56a1499763cff7fb0fba540
Chribtoper <xcstolfax@gmail.com>
2022-02-02 20:24:44 -0700

    SearchResultsPage now checks if a user is logged in and fetches favorites if they are

--- src/pages/SearchResultsPage/SearchResultsPage.js
+++ src/pages/SearchResultsPage/SearchResultsPage.js
@@ -1,6 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import { Outlet } from "react-router-dom";
+import { userActions } from "../../actions/userActions";
+import { getUserSelector } from "../../selectors/userSelectors";
 
-const SearchResultsPage = () => <Outlet />;
+const SearchResultsPage = ({ getFavorites, user: { loggedIn } }) => {
+  React.useEffect(() => {
+    loggedIn && getFavorites();
+  }, [loggedIn]);
+  return <Outlet />;
+};
 
-export default SearchResultsPage;
+SearchResultsPage.propTypes = {
+  getFavorites: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  user: getUserSelector(state)
+});
+
+const actionCreators = {
+  getFavorites: userActions.userGetFavoritedBooks
+};
+
+export default connect(mapStateToProps, actionCreators)(SearchResultsPage);
