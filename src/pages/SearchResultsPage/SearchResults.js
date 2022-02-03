dfee946c74b07ac891b003bcc34533ccac9f6eee
Chribtoper <xcstolfax@gmail.com>
2022-02-02 17:51:31 -0700

    Moved conditional render logic into an if statement for readability

--- src/pages/SearchResultsPage/SearchResults.js
+++ src/pages/SearchResultsPage/SearchResults.js
@@ -7,12 +7,13 @@ import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { searchActions } from "../../actions/searchActions";
+import { getResultsTotalSelector } from "../../selectors/searchSelector";
 import { checkIfLoading } from "../../selectors/uiSelectors";
 import SearchResultLoading from "../../components/Loaders/SearchResultLoading";
 import { searchTypes } from "../../types/searchTypes";
 import { SearchResultPagination } from "../../components/Pagination";
 
-const SearchResults = ({ isLoading, searchBook }) => {
+const SearchResults = ({ isLoading, totalItems, searchBook }) => {
   const { searchQuery, bookId } = useParams();
 
   // When searchResults route is hit, perform a search query
@@ -26,43 +27,45 @@ const SearchResults = ({ isLoading, searchBook }) => {
     searchBook(inputData);
   }, [searchQuery]);
 
-  return (
-    <Box>
-      {isLoading ? (
-        <>
-          <Typography variant="h4">Loading...</Typography>
-          <Stack spacing={2}>
-            {Array.from(
-              { length: 10 },
-              () => new Date().getTime() + Math.random()
-            ).map((key, i) => (
-              <div key={`${key}-${i}`}>
-                <SearchResultLoading key={`${key}-${i}-search-result-card`} />
-                <Divider
-                  sx={{ marginTop: "8px", marginBottom: "8px" }}
-                  key={`${key}-${i}-divider`}
-                />
-              </div>
-            ))}
-          </Stack>
-        </>
-      ) : (
-        <>
-          <Outlet />
-          {!bookId && <SearchResultPagination />}
-        </>
-      )}
-    </Box>
-  );
+  if (isLoading) {
+    return (
+      <Box>
+        <Typography variant="h4">Loading...</Typography>
+        <Stack spacing={2}>
+          {Array.from(
+            { length: 10 },
+            () => new Date().getTime() + Math.random()
+          ).map((key, i) => (
+            <div key={`${key}-${i}`}>
+              <SearchResultLoading key={`${key}-${i}-search-result-card`} />
+              <Divider
+                sx={{ marginTop: "8px", marginBottom: "8px" }}
+                key={`${key}-${i}-divider`}
+              />
+            </div>
+          ))}
+        </Stack>
+      </Box>
+    );
+  } else {
+    return (
+      <Box>
+        <Outlet />
+        {!bookId && <SearchResultPagination />}
+      </Box>
+    );
+  }
 };
 
 SearchResults.propTypes = {
+  totalItems: PropTypes.number.isRequired,
   isLoading: PropTypes.bool.isRequired,
   searchBook: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
-  isLoading: checkIfLoading(state, searchTypes.GET_SEARCH_BOOK_FETCH)
+  isLoading: checkIfLoading(state, searchTypes.GET_SEARCH_BOOK_FETCH),
+  totalItems: getResultsTotalSelector(state)
 });
 
 const actionCreators = {
