2407f5cc1b81425acb812d862e0a636eac37ddf4
Chribtoper <xcstolfax@gmail.com>
2022-02-02 19:10:41 -0700

    Book reducer added to the index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import userReducer from "./userReducer";
 import snackbarReducer from "./snackbarReducer";
 import searchReducer from "./searchReducer";
 import uiReducer from "./uiReducer";
+import bookReducer from "./bookReducer";
 
 const rootReducer = combineReducers({
   userState: userReducer,
   searchState: searchReducer,
   snackbarState: snackbarReducer,
   uiState: uiReducer,
+  bookState: bookReducer
 });
 
 export default rootReducer;
