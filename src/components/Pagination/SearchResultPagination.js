bf7c12c975d75f3a740278f58a9ba3b5234f4a0c
Chribtoper <xcstolfax@gmail.com>
2022-02-02 17:29:05 -0700

    Fixed bugs in my effect hooks, effect hooks now properly change pages based off what page you click and by what the url param is

--- src/components/Pagination/SearchResultPagination.js
+++ src/components/Pagination/SearchResultPagination.js
@@ -11,24 +11,32 @@ const SearchResultPagination = ({ totalItems }) => {
   const pageSize = 10;
   const navigate = useNavigate();
   const { pageNum } = useParams();
-  const [page, setPage] = useState(Number(pageNum) || 1);
+  const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
 
   const handleChange = (e, value) => {
     e.preventDefault();
     setPage(value);
+    // navigate(value);
+    window.scrollTo(0, 0);
   };
 
   useEffect(() => {
+    console.log("Navigating to " + page);
     navigate(`${page}`);
-    window.scrollTo(0, 0);
   }, [page]);
+
+  useEffect(() => {
+    console.log("Setting page to the page num " + pageNum);
+    setPage(parseInt(pageNum));
+  }, [pageNum]);
 
   useEffect(() => {
     let num = Math.floor(totalItems / pageSize);
     if (num < totalItems / pageSize) num = Math.ceil(totalItems / pageSize);
     setTotalPages(num);
   }, [totalItems]);
+
   return (
     <Stack style={{ alignItems: "center" }} spacing={2}>
       <Typography>Page: {page}</Typography>
