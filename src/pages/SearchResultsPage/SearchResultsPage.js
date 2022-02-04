d36eaa83b14dba8a8d488684b04f9b11ef80ee12
Chribtoper <xcstolfax@gmail.com>
2022-02-04 13:43:00 -0700

    No longer grabbing favorites from this component

--- src/pages/SearchResultsPage/SearchResultsPage.js
+++ src/pages/SearchResultsPage/SearchResultsPage.js
@@ -1,27 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
 import { Outlet } from "react-router-dom";
-import { bookActions } from "../../actions/bookActions";
-import { getUserSelector } from "../../selectors/userSelectors";
 
-const SearchResultsPage = ({ getFavorites, user: { loggedIn } }) => {
-  React.useEffect(() => {
-    loggedIn && getFavorites();
-  }, [loggedIn]);
-  return <Outlet />;
-};
+const SearchResultsPage = () => <Outlet />;
 
-SearchResultsPage.propTypes = {
-  getFavorites: PropTypes.func.isRequired
-};
-
-const mapStateToProps = state => ({
-  user: getUserSelector(state)
-});
-
-const actionCreators = {
-  getFavorites: bookActions.getFavoritedBooks
-};
-
-export default connect(mapStateToProps, actionCreators)(SearchResultsPage);
+export default SearchResultsPage;
