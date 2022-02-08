720b539d1b19204bf0f822929852ee4095f0e8cc
Chribtoper <xcstolfax@gmail.com>
2022-02-08 02:01:18 -0700

    If there is a book ID, only render the Outlet (FavoriteBookPage)

--- src/pages/FavoritesPage/FavoriteBooks.js
+++ src/pages/FavoritesPage/FavoriteBooks.js
@@ -10,12 +10,12 @@ import { userTypes } from "../../types/userTypes";
 import { useNavigate, useParams } from "react-router-dom";
 import {
   getFavoritesSelector,
-  getTotalFavoritesSelector
+  getTotalFavoritesSelector,
 } from "../../selectors/bookSelector";
 import {
   GET_FAVORITED_BOOKS_FETCH,
   GET_SAVE_FAVORITED_BOOK_FETCH,
-  GET_REMOVE_FAVORITED_BOOK_FETCH
+  GET_REMOVE_FAVORITED_BOOK_FETCH,
 } from "../../types/bookTypes";
 import Divider from "@mui/material/Divider";
 import Stack from "@mui/material/Stack";
@@ -32,7 +32,7 @@ const FavoriteBooks = ({
   userRemoveFavoritedBook,
   favorites,
   totalItems,
-  user: { loggedIn }
+  user: { loggedIn },
 }) => {
   const navigate = useNavigate();
   const { pageNum, bookId } = useParams();
@@ -41,8 +41,6 @@ const FavoriteBooks = ({
   const getStartIndex = () => (pageNum - 1) * pageSize;
 
   const getEndIndex = () => Math.min(getStartIndex() + pageSize, totalItems);
-
-  useEffect(() => {}, []);
 
   if (isFavoritesLoading) {
     return (
@@ -64,6 +62,8 @@ const FavoriteBooks = ({
         </Stack>
       </Box>
     );
+  } else if (bookId) {
+    return <Outlet />;
   } else {
     return (
       <Box>
@@ -73,7 +73,7 @@ const FavoriteBooks = ({
           <>
             <Typography variant="h4">Favorites:</Typography>
             <Stack spacing={2}>
-              {favorites.slice(getStartIndex(), getEndIndex()).map(book => (
+              {favorites.slice(getStartIndex(), getEndIndex()).map((book) => (
                 <div key={`${book.googleBooksId}-favorite-book-div`}>
                   <BookCard
                     key={`${book.googleBooksId}-favorite-book-card`}
@@ -101,17 +101,17 @@ FavoriteBooks.propTypes = {
     PropTypes.shape({ googleBooksId: PropTypes.string })
   ),
   user: PropTypes.shape({
-    loggedIn: PropTypes.bool.isRequired
-  }).isRequired
+    loggedIn: PropTypes.bool.isRequired,
+  }).isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   favorites: getFavoritesSelector(state),
   totalItems: getTotalFavoritesSelector(state),
   user: getUserSelector(state),
   isAddFavLoading: checkIfLoading(state, GET_SAVE_FAVORITED_BOOK_FETCH),
   isDelFavLoading: checkIfLoading(state, GET_REMOVE_FAVORITED_BOOK_FETCH),
-  isFavoritesLoading: checkIfLoading(state, GET_FAVORITED_BOOKS_FETCH)
+  isFavoritesLoading: checkIfLoading(state, GET_FAVORITED_BOOKS_FETCH),
 });
 
 const actionCreators = {};
