3517412e03dca494ef413b7fc94fa56bdaefc8d5
Chribtoper <xcstolfax@gmail.com>
2022-02-08 02:05:47 -0700

    Book result page now has a firstRender state, this state starts on true until it retrieves some sort of data in selectedBook then it is set to false. When firstRender is set to false, it checks whether or not selectedBook is empty. If empty, navigate to an error page, otherwise check if the item is in the favorites as a final step

--- src/pages/SearchResultsPage/BookResultPage.js
+++ src/pages/SearchResultsPage/BookResultPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import PropTypes from "prop-types";
 import { routes } from "../../config";
 import { connect } from "react-redux";
@@ -21,12 +21,12 @@ import IconButton from "@mui/material/IconButton";
 import { bookActions } from "../../actions/bookActions";
 import {
   GET_SAVE_FAVORITED_BOOK_FETCH,
-  GET_REMOVE_FAVORITED_BOOK_FETCH
+  GET_REMOVE_FAVORITED_BOOK_FETCH,
 } from "../../types/bookTypes";
 import getUserSelector from "../../selectors/userSelectors";
 import {
   getFavoritesSelector,
-  getSelectedBookSelector
+  getSelectedBookSelector,
 } from "../../selectors/bookSelector";
 
 const BookResultPage = ({
@@ -40,35 +40,64 @@ const BookResultPage = ({
   favorites,
   selectedBook,
   user: { loggedIn },
-  searchResults: { bookSearchData }
+  searchResults: { bookSearchData },
 }) => {
   const navigate = useNavigate();
   const { bookId } = useParams();
+  const [firstRender, setFirstRender] = useState(true);
   const [isFavorited, setIsFavorited] = useState(false);
 
   useEffect(() => {
+    if (!firstRender) {
+      console.log(
+        "Either a book was selected or there is no book data anywhere"
+      );
+      if (isEmpty(selectedBook)) {
+        console.log(`No book with id: ${bookId}`);
+        navigate("error-404", { replace: true });
+      } else {
+        console.log("Book exists, now we check if its in the favorites");
+
+        favorites.find((book) => book.googleBooksId === bookId) &&
+          setIsFavorited(true);
+      }
+    } else {
+      console.log("First render, so skip until next call");
+    }
+  }, [firstRender]);
+
+  useEffect(() => {
     setIsFavorited(
-      favorites &&
-        favorites.some(
-          favoritedBook =>
-            favoritedBook.googleBooksId === selectedBook.googleBooksId
-        )
+      favorites.some(
+        (favoritedBook) =>
+          favoritedBook.googleBooksId === selectedBook.googleBooksId
+      )
     );
   }, [favorites]);
 
-  // When a user exists this page or backs out of it, reset SearchResultsPage selectedBook state back to its initial state.
-  useEffect(() => {
+  useEffect(async () => {
     if (isEmpty(bookSearchData)) {
+      console.log("No search data, manually fetch for book data");
       const inputData = { data: JSON.stringify({ googleBooksId: bookId }) };
-      searchBookById(inputData);
+      await searchBookById(inputData);
+    } else {
+      console.log(
+        "There is search data, try to find the book by ID in search data."
+      );
+      const book = bookSearchData.find((book) => book.googleBooksId === bookId);
+      setSelectedBook({
+        message: book ? "Book selected" : "Book doesn't exist",
+        selectedBook: book || {},
+      });
     }
+    setFirstRender(false);
     return () => {
       setSelectedBook({
         message: "Removed selected book",
-        selectedBook: {}
+        selectedBook: {},
       });
     };
-  }, []);
+  }, [bookId]);
 
   const handleOnClickFavorite = () => {
     if (!loggedIn) {
@@ -88,20 +117,21 @@ const BookResultPage = ({
       return;
     } else {
       const inputData = {
-        data: { bookData: JSON.stringify(selectedBook) }
+        data: { bookData: JSON.stringify(selectedBook) },
       };
       removeFavoritedBook(inputData);
     }
   };
 
   // TODO: Better handling of if the ID does not exist
-  const isEmpty = obj => {
+  const isEmpty = (obj) => {
     if (Object.keys(obj).length === 0) {
       return true;
     } else {
       return false;
     }
   };
+
   return (
     <Box>
       <CssBaseline />
@@ -122,7 +152,7 @@ const BookResultPage = ({
                   width: 128,
                   minWidth: 128,
                   height: 168,
-                  minHeight: 168
+                  minHeight: 168,
                 }}
               />
             </div>
@@ -130,7 +160,8 @@ const BookResultPage = ({
               <Rating
                 style={{ marginTop: "8px" }}
                 name="half-rating-read"
-                defaultValue={selectedBook.averageRating}
+                defaultValue={0}
+                value={selectedBook.averageRating || 0}
                 precision={0.5}
                 readOnly
               />
@@ -167,7 +198,7 @@ const BookResultPage = ({
             <Typography component="div" variant="h5">
               {selectedBook.title}
             </Typography>
-            {selectedBook.authors.map(author => (
+            {selectedBook.authors.map((author) => (
               <Typography
                 key={author}
                 variant="subtitle1"
@@ -216,26 +247,28 @@ BookResultPage.propTypes = {
       ratingsCount: PropTypes.number,
       imageLink: PropTypes.string,
       language: PropTypes.string,
-      categories: PropTypes.arrayOf(PropTypes.string)
-    })
-  }).isRequired
+      categories: PropTypes.arrayOf(PropTypes.string),
+    }),
+  }).isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   favorites: getFavoritesSelector(state),
   user: getUserSelector(state),
   searchResults: getSearchSelector(state),
   selectedBook: getSelectedBookSelector(state),
-  isLoading: checkIfLoading(state, searchTypes.GET_SEARCH_BOOK_BY_ID_FETCH),
+  isLoading:
+    checkIfLoading(state, searchTypes.GET_SEARCH_BOOK_BY_ID_FETCH) ||
+    checkIfLoading(state, searchTypes.GET_SEARCH_BOOK_BY_ID_FETCH),
   isAddFavLoading: checkIfLoading(state, GET_SAVE_FAVORITED_BOOK_FETCH),
-  isDelFavLoading: checkIfLoading(state, GET_REMOVE_FAVORITED_BOOK_FETCH)
+  isDelFavLoading: checkIfLoading(state, GET_REMOVE_FAVORITED_BOOK_FETCH),
 });
 
 const actionCreators = {
   searchBookById: bookActions.searchBookById,
   setSelectedBook: bookActions.setSelectedBook,
   saveFavoritedBook: bookActions.saveFavoritedBook,
-  removeFavoritedBook: bookActions.removeFavoritedBook
+  removeFavoritedBook: bookActions.removeFavoritedBook,
 };
 
 export default connect(mapStateToProps, actionCreators)(BookResultPage);
