6307ac71032ba6bed192c9076884751ffbf4df5e
Chribtoper <xcstolfax@gmail.com>
2022-02-08 02:06:53 -0700

    This page checks if the book exists in favorites and renders it otherwise navigates to error page

---
+++ src/pages/FavoritesPage/FavoriteBookPage.js
@@ -0,0 +1,221 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { routes } from "../../config";
+import { connect } from "react-redux";
+import { searchTypes } from "../../types/searchTypes";
+import { getSearchSelector } from "../../selectors/searchSelector";
+import { useParams, useNavigate } from "react-router-dom";
+import Typography from "@mui/material/Typography";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import BookIcon from "@mui/icons-material/Book";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { checkIfLoading } from "../../selectors/uiSelectors";
+import CssBaseline from "@mui/material/CssBaseline";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
+import IconButton from "@mui/material/IconButton";
+import { bookActions } from "../../actions/bookActions";
+import {
+  GET_SAVE_FAVORITED_BOOK_FETCH,
+  GET_REMOVE_FAVORITED_BOOK_FETCH,
+} from "../../types/bookTypes";
+import getUserSelector from "../../selectors/userSelectors";
+import {
+  getFavoritesSelector,
+  getSelectedBookSelector,
+} from "../../selectors/bookSelector";
+
+const BookResultPage = ({
+  saveFavoritedBook,
+  removeFavoritedBook,
+  isDelFavLoading,
+  isAddFavLoading,
+  isLoading,
+  setSelectedBook,
+  favorites,
+  selectedBook,
+  user: { loggedIn },
+}) => {
+  const navigate = useNavigate();
+  const { bookId } = useParams();
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  useEffect(() => {
+    setIsFavorited(
+      favorites &&
+        favorites.some(
+          (favoritedBook) =>
+            favoritedBook.googleBooksId === selectedBook.googleBooksId
+        )
+    );
+  }, [favorites]);
+
+  // When a user exists this page or backs out of it, reset SearchResultsPage selectedBook state back to its initial state.
+  useEffect(() => {
+    const book = favorites.find((book) => book.googleBooksId === bookId);
+    if (book) {
+      setIsFavorited(true);
+      setSelectedBook({ message: "Book selected", selectedBook: book || {} });
+    } else {
+      navigate("error-404", { replace: true });
+    }
+    return () => {
+      setSelectedBook({
+        message: "Removed selected book",
+        selectedBook: {},
+      });
+    };
+  }, [bookId]);
+
+  const handleOnClickFavorite = () => {
+    if (!loggedIn) {
+      navigate(routes.signIn);
+    } else if (isAddFavLoading || isDelFavLoading) {
+      return;
+    } else {
+      const inputData = { data: JSON.stringify(selectedBook) };
+      saveFavoritedBook(inputData);
+    }
+  };
+
+  const handleOnClickRemoveFavorite = () => {
+    if (!loggedIn) {
+      navigate(routes.signIn);
+    } else if (isAddFavLoading || isDelFavLoading) {
+      return;
+    } else {
+      const inputData = {
+        data: { bookData: JSON.stringify(selectedBook) },
+      };
+      removeFavoritedBook(inputData);
+    }
+  };
+
+  // TODO: Better handling of if the ID does not exist
+  const isEmpty = (obj) => {
+    if (Object.keys(obj).length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  return (
+    <Box>
+      <CssBaseline />
+      {isLoading && isEmpty(selectedBook) && (
+        <Typography component="div" variant="h5">
+          Loading...
+        </Typography>
+      )}
+      {!isLoading && !isEmpty(selectedBook) && (
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Box sx={{ p: 1, display: "inline-block", verticalAlign: "middle" }}>
+            <div>
+              <Box
+                component="img"
+                src={selectedBook.imageLink}
+                alt={selectedBook.title}
+                sx={{
+                  width: 128,
+                  minWidth: 128,
+                  height: 168,
+                  minHeight: 168,
+                }}
+              />
+            </div>
+            <div>
+              <Rating
+                style={{ marginTop: "8px" }}
+                name="half-rating-read"
+                defaultValue={selectedBook.averageRating}
+                precision={0.5}
+                readOnly
+              />
+              <Typography variant="caption" display="block" gutterBottom>
+                {selectedBook.ratingsCount > 0 &&
+                  `Ratings: ${selectedBook.ratingsCount}`}
+              </Typography>
+            </div>
+            <div>
+              {favorites && isFavorited ? (
+                <IconButton
+                  onClick={handleOnClickRemoveFavorite}
+                  aria-label="favorite-item"
+                >
+                  <FavoriteIcon color="secondary" />
+                </IconButton>
+              ) : (
+                <IconButton
+                  onClick={handleOnClickFavorite}
+                  aria-label="favorite-item"
+                >
+                  <FavoriteBorderIcon />
+                </IconButton>
+              )}
+              <IconButton aria-label="currently-reading">
+                <AutoStoriesIcon />
+              </IconButton>
+              <IconButton aria-label="finished-reading">
+                <BookIcon />
+              </IconButton>
+            </div>
+          </Box>
+          <Box sx={{ p: 1, display: "inline-block", verticalAlign: "middle" }}>
+            <Typography component="div" variant="h5">
+              {selectedBook.title}
+            </Typography>
+            {selectedBook.authors.map((author) => (
+              <Typography
+                key={author}
+                variant="subtitle1"
+                color="text.secondary"
+                component="div"
+              >
+                {author}
+              </Typography>
+            ))}
+            <Typography
+              variant="subtitle2"
+              color="text.secondary"
+              component="div"
+            >
+              {selectedBook.description}
+            </Typography>
+          </Box>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+BookResultPage.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  setSelectedBook: PropTypes.func.isRequired,
+  saveFavoritedBook: PropTypes.func.isRequired,
+  removeFavoritedBook: PropTypes.func.isRequired,
+  isAddFavLoading: PropTypes.bool.isRequired,
+  isDelFavLoading: PropTypes.bool.isRequired,
+  favorites: PropTypes.arrayOf(
+    PropTypes.shape({ googleBooksId: PropTypes.string })
+  ).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  favorites: getFavoritesSelector(state),
+  user: getUserSelector(state),
+  selectedBook: getSelectedBookSelector(state),
+  isLoading: checkIfLoading(state, searchTypes.GET_SEARCH_BOOK_BY_ID_FETCH),
+  isAddFavLoading: checkIfLoading(state, GET_SAVE_FAVORITED_BOOK_FETCH),
+  isDelFavLoading: checkIfLoading(state, GET_REMOVE_FAVORITED_BOOK_FETCH),
+});
+
+const actionCreators = {
+  setSelectedBook: bookActions.setSelectedBook,
+  saveFavoritedBook: bookActions.saveFavoritedBook,
+  removeFavoritedBook: bookActions.removeFavoritedBook,
+};
+
+export default connect(mapStateToProps, actionCreators)(BookResultPage);
