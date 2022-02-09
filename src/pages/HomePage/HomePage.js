5b0a19034a50408d1753cfc8d64d72979019a684
Chribtoper <xcstolfax@gmail.com>
2022-02-08 23:42:59 -0700

    HomePage now has has a SearchBar and a title

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,23 +1,49 @@
 import React from "react";
 import { styled, alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import TextField from "@mui/material/TextField";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import DirectionsIcon from "@mui/icons-material/Directions";
+import Typography from "@mui/material/Typography";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 const Background = styled("div")(({ theme }) => ({
-  position: "absolute",
-  backgroundImage: `url(${process.env.PUBLIC_URL}/library.svg)`,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  boxSizing: "border-box",
+  borderRadius: "3px",
+  width: "100%",
+  minHeight: "200px%",
+  backgroundColor: "#E6E7E8",
+  color: "#333333",
+  textAlign: "center",
+  padding: "40px 15px 500px",
+  // backgroundImage: `url(${process.env.PUBLIC_URL}/library.svg)`,
   backgroundPosition: "center center",
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover",
-  backgroundAttachment: "fixed",
-  height: "100%",
-  width: "100%",
-  overflow: "hidden",
-  bottom: "0px",
-  left: "0px",
-  right: "0px"
 }));
-
 const HomePage = (props) => {
-  return <Background />;
+  return (
+    <Background>
+      <Typography variant="h3" gutterBottom>
+        Discover books you'll love!
+      </Typography>
+      <SearchBar />
+    </Background>
+  );
 };
 
 export default HomePage;
