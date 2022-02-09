b58e7bf0bc21606be491d7c54bac219cc3064103
Chribtoper <xcstolfax@gmail.com>
2022-02-09 14:52:23 -0700

    Created quote types

---
+++ src/types/quoteTypes.js
@@ -0,0 +1,5 @@
+// Fetch a random quote of the day
+export const GET_RANDOM_QUOTE_FETCH = "GET_RANDOM_QUOTE_FETCH";
+
+// Random quote fetch receives data successfully
+export const GET_RANDOM_QUOTE_SUCCESS = "GET_RANDOM_QUOTE_SUCCESS";
