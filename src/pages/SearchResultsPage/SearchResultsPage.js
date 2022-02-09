5bfda9d6e038d4cf951b4c7343571f1acf8683df
Chribtoper <xcstolfax@gmail.com>
2022-02-09 13:28:51 -0700

    SearchResultsPage now renders a SearchBar component

--- src/pages/SearchResultsPage/SearchResultsPage.js
+++ src/pages/SearchResultsPage/SearchResultsPage.js
@@ -1,6 +1,15 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import SearchBar from "../../components/SearchBar/SearchBar";
 import { Outlet } from "react-router-dom";
 
-const SearchResultsPage = () => <Outlet />;
+const SearchResultsPage = () => (
+  <Box>
+    <Box sx={{ textAlign: "center" }}>
+      <SearchBar />
+    </Box>
+    <Outlet />
+  </Box>
+);
 
 export default SearchResultsPage;
