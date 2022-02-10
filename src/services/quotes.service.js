82f32e0c05d43425de50f84be266bd81752a2557
Chribtoper <xcstolfax@gmail.com>
2022-02-10 03:15:54 -0700

    Handles fetching from my quotes api

--- src/services/quotes.service.js
+++ src/services/quotes.service.js
@@ -1,13 +1,13 @@
 import Axios from "axios";
 import { endPoints, controllers } from "../config";
 
-const getRandomQod = async () => {
+const fetchRandomQoute = async () => {
   Axios.defaults.withCredentials = true;
   return await Axios.get(
     `${process.env.REACT_APP_API_URI}/${controllers.quotes}/${endPoints.getRandomQod}`
   );
 };
 
-export const quotesService = {
-  getRandomQod,
+export const quoteService = {
+  fetchRandomQoute,
 };
