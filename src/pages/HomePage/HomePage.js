e465860b27dbdb980502ae44a159b1c81448b392
Chribtoper <xcstolfax@gmail.com>
2022-02-10 16:31:51 -0700

    Now wrapped with a Container component, has a library image rendered at the bottom

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,30 +1,17 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { styled, alpha } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import InputBase from "@mui/material/InputBase";
-import TextField from "@mui/material/TextField";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import DirectionsIcon from "@mui/icons-material/Directions";
+import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { quoteActions } from "../../actions/quoteActions";
 import { getSelectedQuoteSelector } from "../../selectors/quoteSelector";
 import { GET_RANDOM_QUOTE_SUCCESS } from "../../types/quoteTypes";
 import { checkIfLoading } from "../../selectors/uiSelectors";
 
-const Background = styled("div")(({ theme }) => ({
+const HomeContainer = styled(Container)(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
@@ -32,23 +19,26 @@ const Background = styled("div")(({ theme }) => ({
   borderRadius: "3px",
   width: "100%",
   minHeight: "200px%",
-  backgroundColor: "#E6E7E8",
+  backgroundColor: "#FDF4E7",
   color: "#333333",
   textAlign: "center",
-  padding: "40px 15px 500px",
-  // backgroundImage: `url(${process.env.PUBLIC_URL}/library.svg)`,
-  backgroundPosition: "center center",
-  backgroundRepeat: "no-repeat",
-  backgroundSize: "cover",
+  padding: "40px 15px 200px",
 }));
+
+const LibaryImage = styled("img")(({ theme }) => ({
+  minHeight: "300px",
+  maxHeight: "500px",
+  width: "100%",
+}));
+
 const HomePage = ({ getRandomQuote, selectedQuote, isLoading }) => {
   useEffect(() => {
     getRandomQuote();
   }, []);
 
   return (
-    <Background>
-      <Box>
+    <div style={{ backgroundColor: "#FDF4E7" }}>
+      <HomeContainer>
         <Typography fontWeight="bold" variant="h4" gutterBottom>
           Discover and save books you love!
         </Typography>
@@ -59,14 +49,20 @@ const HomePage = ({ getRandomQuote, selectedQuote, isLoading }) => {
           favorites!
         </Typography>
         <SearchBar />
-        <Typography fontWeight="bold" variant="h5" gutterBottom>
+        <Typography
+          sx={{ fontSize: "1rem" }}
+          fontWeight="bold"
+          variant="overline"
+          gutterBottom
+        >
           {selectedQuote.text}
         </Typography>
         <Typography fontWeight="bold" variant="caption" gutterBottom>
           -{selectedQuote.author}
         </Typography>
-      </Box>
-    </Background>
+        <LibaryImage src={`${process.env.PUBLIC_URL}/library2.svg`} />
+      </HomeContainer>
+    </div>
   );
 };
 
