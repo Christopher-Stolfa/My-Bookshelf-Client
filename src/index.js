07a3f9c691185cf786ee9e1afe1f2e66923af010
Chribtoper <xcstolfax@gmail.com>
2022-02-11 01:48:31 -0700

    Sets the background color of the body to a cream color

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import { Provider } from "react-redux";
 import SnackbarNotifier from "./components/SnackbarNotifier/SnackbarNotifier";
 import store from "./store";
 
+document.body.style = "background-color: #FDF4E7";
+
 ReactDOM.render(
   <Provider store={store}>
     <ThemeProvider theme={libraryTheme}>
