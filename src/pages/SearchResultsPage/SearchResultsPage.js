bcf717c5d46d9b5de2d93906795400d369abe9fe
Chribtoper <xcstolfax@gmail.com>
2022-02-11 01:51:23 -0700

    Changed the background color but then removed it since the body now handles it

--- src/pages/SearchResultsPage/SearchResultsPage.js
+++ src/pages/SearchResultsPage/SearchResultsPage.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { Outlet } from "react-router-dom";
 
 const SearchResultsPage = () => (
-  <Box>
+  <Container sx={{ padding: "25px" }}>
     <Box sx={{ textAlign: "center" }}>
       <SearchBar />
     </Box>
     <Outlet />
-  </Box>
+  </Container>
 );
 
 export default SearchResultsPage;
