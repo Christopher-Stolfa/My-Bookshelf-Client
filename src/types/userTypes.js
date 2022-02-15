de8bc69b9548c243ae1f192f9025cb883a9fbc43
Chribtoper <xcstolfax@gmail.com>
2022-02-15 16:42:05 -0700

    Created TOKEN RESET types

--- src/types/userTypes.js
+++ src/types/userTypes.js
@@ -28,6 +28,12 @@ const GET_FORGOT_PASSWORD_FETCH = "GET_FORGOT_PASSWORD_FETCH";
 // Forgot password success
 const GET_FORGOT_PASSWORD_SUCCESS = "GET_FORGOT_PASSWORD_SUCCESS";
 
+// User is accessing a password reset page which needs token verification
+const GET_CHECK_RESET_TOKEN_FETCH = "GET_CHECK_RESET_TOKEN_FETCH";
+
+// User receives a successful response from the server checking if token is valid
+const GET_CHECK_RESET_TOKEN_SUCCESS = "GET_CHECK_RESET_TOKEN_SUCCESS";
+
 export const userTypes = {
   GET_SESSION_FETCH,
   GET_SESSION_SUCCESS,
@@ -39,4 +45,6 @@ export const userTypes = {
   GET_SIGNOUT_SUCCESS,
   GET_FORGOT_PASSWORD_FETCH,
   GET_FORGOT_PASSWORD_SUCCESS,
+  GET_CHECK_RESET_TOKEN_FETCH,
+  GET_CHECK_RESET_TOKEN_SUCCESS,
 };
