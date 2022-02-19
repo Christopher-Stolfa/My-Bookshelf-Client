f1c79f8a5f4a13e566028d7ccd00aef000863fcd
Chribtoper <xcstolfax@gmail.com>
2022-02-19 12:57:49 -0700

    Popover now works for sortbutton TODO: Create sorting methods

--- src/components/Buttons/SortButton.js
+++ src/components/Buttons/SortButton.js
@@ -1,17 +1,104 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { bookActions } from "../../actions/bookActions";
 import SortIcon from "@mui/icons-material/Sort";
-import IconButton from "@mui/material/IconButton";
+import Button from "@mui/material/Button";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Grow from "@mui/material/Grow";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import MenuItem from "@mui/material/MenuItem";
+import MenuList from "@mui/material/MenuList";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
 // To make this component reusable, the parent component will pass its items as the prop "items"
 // The type of items of the parent are also passed down
 const SortButton = ({ items, type }) => {
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
+
+  const prevOpen = useRef(open);
+  useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleListKeyDown = (event) => {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  };
+
   return (
-    <IconButton>
-      <SortIcon />
-    </IconButton>
+    <Stack direction="row" spacing={2}>
+      <div>
+        <Button
+          variant="text"
+          startIcon={<SortIcon />}
+          ref={anchorRef}
+          id="composition-button"
+          aria-controls={open ? "composition-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+        >
+          SORT
+        </Button>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          placement="bottom-start"
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom-start" ? "left top" : "left bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="composition-menu"
+                    aria-labelledby="composition-button"
+                    onKeyDown={handleListKeyDown}
+                  >
+                    <MenuItem onClick={handleClose}>test1</MenuItem>
+                    <MenuItem onClick={handleClose}>test2</MenuItem>
+                    <MenuItem onClick={handleClose}>test3</MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </div>
+    </Stack>
   );
 };
 
