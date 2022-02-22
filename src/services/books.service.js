6fb22d42bd37e51aa579cf09b221a6f4c4048bd4
Chribtoper <xcstolfax@gmail.com>
2022-02-21 20:42:37 -0700

    Added requests for dealing with notes

--- src/services/books.service.js
+++ src/services/books.service.js
@@ -40,10 +40,46 @@ const removeFavoritedBook = async (inputData) => {
   );
 };
 
+const saveNote = async (inputData) => {
+  Axios.defaults.withCredentials = true;
+  return await Axios.post(
+    `${process.env.REACT_APP_API_URI}/${controllers.books}/${endPoints.saveNote}`,
+    inputData
+  );
+};
+
+const editNote = async (inputData) => {
+  Axios.defaults.withCredentials = true;
+  return await Axios.put(
+    `${process.env.REACT_APP_API_URI}/${controllers.books}/${endPoints.editNote}`,
+    inputData
+  );
+};
+
+const deleteNote = async (inputData) => {
+  Axios.defaults.withCredentials = true;
+  return await Axios.delete(
+    `${process.env.REACT_APP_API_URI}/${controllers.books}/${endPoints.deleteNote}`,
+    inputData
+  );
+};
+
+const getNotes = async (inputData) => {
+  Axios.defaults.withCredentials = true;
+  return await Axios.get(
+    `${process.env.REACT_APP_API_URI}/${controllers.books}/${endPoints.getNotes}`,
+    inputData
+  );
+};
+
 export const bookService = {
   searchBook,
   searchBookById,
   saveFavoritedBook,
   removeFavoritedBook,
   getFavoritedBooks,
+  saveNote,
+  editNote,
+  deleteNote,
+  getNotes,
 };
