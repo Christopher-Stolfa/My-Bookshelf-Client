a98eb577b01e29634e49545f462fda1575a92ff1
Chribtoper <xcstolfax@gmail.com>
2022-02-26 23:02:35 -0700

    Added comments for uiActions

--- src/actions/uiActions.js
+++ src/actions/uiActions.js
@@ -1,5 +1,11 @@
 import { uiTypes } from "../types/uiTypes";
 
+/**
+ * This action is triggered when a component triggers the start of fetch request related action.
+ * @param {string} name - The type of fetch in progress.
+ * @param {object} params - Extra params associated with the action if needed.
+ * @returns {object} - An object containing a type and payload to the reducers.
+ */
 export const startAction = (name, params) => ({
   type: uiTypes.START_ACTION,
   payload: {
@@ -7,6 +13,12 @@ export const startAction = (name, params) => ({
   },
 });
 
+/**
+ * This action is triggered when a component triggers the end of fetch request related action.
+ * @param {string} name - The type of fetch in progress.
+ * @param {object} params - Extra params associated with the action if needed.
+ * @returns {object} - An object containing a type and payload to the reducers.
+ */
 export const stopAction = (name, params) => ({
   type: uiTypes.STOP_ACTION,
   payload: {
@@ -14,11 +26,23 @@ export const stopAction = (name, params) => ({
   },
 });
 
+/**
+ * This action is triggered when a component triggers the start of a refresh related action.
+ * @param {string} name - The type of fetch in progress.
+ * @param {object} params - Extra params associated with the action if needed.
+ * @returns {object} - An object containing a type and payload to the reducers.
+ */
 export const refreshActionStart = (refreshAction) => ({
   type: uiTypes.REFRESH_ACTION_START,
   payload: { refreshAction },
 });
 
+/**
+ * This action is triggered when a component triggers the end of a refresh related action.
+ * @param {string} name - The type of fetch in progress.
+ * @param {object} params - Extra params associated with the action if needed.
+ * @returns {object} - An object containing a type and payload to the reducers.
+ */
 export const refreshActionStop = (refreshAction) => ({
   type: uiTypes.REFRESH_ACTION_STOP,
   payload: { refreshAction },
