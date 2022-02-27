0a493560ee81788238fb93f9e6918ce1b8e168d7
Chribtoper <xcstolfax@gmail.com>
2022-02-27 15:20:53 -0700

    When the screen size gets small, render a menu button with a menulist instead of buttons for navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,79 +1,251 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
-import Toolbar from "@mui/material/Toolbar";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import AppBar from "@mui/material/AppBar";
+import {
+  Toolbar,
+  Button,
+  Typography,
+  AppBar,
+  Stack,
+  Popper,
+  Grow,
+  Paper,
+  ClickAwayListener,
+  MenuList,
+  MenuItem,
+  useMediaQuery,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
 import { connect } from "react-redux";
 import { getUserSelector } from "../../selectors/userSelectors";
 import { userActions } from "../../actions/userActions";
 import { routes } from "../../config";
 
-const Navigation = (props) => {
+const Navigation = ({ user: { loggedIn }, signOut }) => {
   const navigate = useNavigate();
+  const anchorRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
+  const prevOpen = useRef(open);
+  useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleListKeyDown = (event) => {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  };
+
   const handleOnClick = ({ target: { name } }) => {
     const path = name === routes.favorites ? `${name}/1` : name;
     navigate(path);
   };
-  const handleSignout = (e) => {
-    props.signOut();
+
+  const handleOnMenuClick = (e, route) => {
+    if (anchorRef.current && anchorRef.current.contains(e.target)) {
+      return;
+    }
+    const path = route === routes.favorites ? `${route}/1` : route;
+    setOpen(false);
+    navigate(path);
   };
+  const handleSignout = () => signOut();
 
   return (
     <AppBar>
-      <Toolbar>
-        <Typography
-          variant="h6"
-          noWrap
-          component="div"
-          sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-        >
-          My Bookshelf
-        </Typography>
-        <Button onClick={handleOnClick} name={routes.home} color="inherit">
-          Home
-        </Button>
-        {props.user.loggedIn ? (
-          <>
+      <Toolbar sx={{ display: "flex" }}>
+        {isMobile ? (
+          <Stack direction="row" spacing={2}>
             <Button
-              onClick={handleOnClick}
-              name={routes.favorites}
               color="inherit"
+              sx={{ fontSize: "1rem" }}
+              variant="text"
+              startIcon={<MenuIcon />}
+              ref={anchorRef}
+              id="menu-button"
+              aria-controls={open ? "route-menu" : undefined}
+              aria-expanded={open ? "true" : undefined}
+              aria-haspopup="true"
+              onClick={handleToggle}
             >
-              Favorites
+              MENU
             </Button>
-            <Button
-              onClick={handleOnClick}
-              name={routes.account}
-              color="inherit"
+            <Popper
+              open={open}
+              anchorEl={anchorRef.current}
+              role={undefined}
+              placement="bottom-start"
+              transition
+              disablePortal
+              style={{ zIndex: 1 }}
             >
-              Account
-            </Button>
-            <Button
-              onClick={handleSignout}
-              name={routes.signOut}
-              color="inherit"
-            >
-              Sign Out
-            </Button>
-          </>
+              {({ TransitionProps, placement }) => (
+                <Grow
+                  {...TransitionProps}
+                  style={{
+                    transformOrigin:
+                      placement === "bottom-start" ? "left top" : "left bottom",
+                  }}
+                >
+                  <Paper>
+                    <ClickAwayListener onClickAway={handleClose}>
+                      <MenuList
+                        autoFocusItem={open}
+                        id="route-menu"
+                        aria-labelledby="route-button"
+                        onKeyDown={handleListKeyDown}
+                      >
+                        <MenuItem
+                          onClick={(e) => handleOnMenuClick(e, routes.home)}
+                          key={routes.home}
+                          color="inherit"
+                        >
+                          Home
+                        </MenuItem>
+                        <MenuItem
+                          onClick={(e) => handleOnMenuClick(e, routes.about)}
+                          key={routes.about}
+                          color="inherit"
+                        >
+                          About
+                        </MenuItem>
+                        {loggedIn
+                          ? [
+                              <MenuItem
+                                onClick={(e) =>
+                                  handleOnMenuClick(e, routes.favorites)
+                                }
+                                key={routes.favorites}
+                                color="inherit"
+                              >
+                                Favorites
+                              </MenuItem>,
+                              <MenuItem
+                                onClick={(e) =>
+                                  handleOnMenuClick(e, routes.account)
+                                }
+                                key={routes.account}
+                                color="inherit"
+                              >
+                                Account
+                              </MenuItem>,
+                              <MenuItem
+                                onClick={handleSignout}
+                                key={routes.signOut}
+                                color="inherit"
+                              >
+                                Sign Out
+                              </MenuItem>,
+                            ]
+                          : [
+                              <MenuItem
+                                onClick={(e) =>
+                                  handleOnMenuClick(e, routes.signIn)
+                                }
+                                key={routes.signIn}
+                                color="inherit"
+                              >
+                                Sign In
+                              </MenuItem>,
+                              <MenuItem
+                                onClick={(e) =>
+                                  handleOnMenuClick(e, routes.signUp)
+                                }
+                                key={routes.signUp}
+                                color="inherit"
+                              >
+                                Sign Up
+                              </MenuItem>,
+                            ]}
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+              )}
+            </Popper>
+          </Stack>
         ) : (
           <>
-            <Button
-              onClick={handleOnClick}
-              name={routes.signIn}
-              color="inherit"
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{
+                flexGrow: 1,
+                display: "block",
+              }}
             >
-              Sign In
+              My Bookshelf
+            </Typography>
+            <Button onClick={handleOnClick} name={routes.home} color="inherit">
+              Home
             </Button>
-            <Button
-              onClick={handleOnClick}
-              name={routes.signUp}
-              color="inherit"
-            >
-              Sign Up
+            <Button onClick={handleOnClick} name={routes.about} color="inherit">
+              About
             </Button>
+            {loggedIn ? (
+              <>
+                <Button
+                  onClick={handleOnClick}
+                  name={routes.favorites}
+                  color="inherit"
+                >
+                  Favorites
+                </Button>
+                <Button
+                  onClick={handleOnClick}
+                  name={routes.account}
+                  color="inherit"
+                >
+                  Account
+                </Button>
+                <Button
+                  onClick={handleSignout}
+                  name={routes.signOut}
+                  color="inherit"
+                >
+                  Sign Out
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button
+                  onClick={handleOnClick}
+                  name={routes.signIn}
+                  color="inherit"
+                >
+                  Sign In
+                </Button>
+                <Button
+                  onClick={handleOnClick}
+                  name={routes.signUp}
+                  color="inherit"
+                >
+                  Sign Up
+                </Button>
+              </>
+            )}
           </>
         )}
       </Toolbar>
