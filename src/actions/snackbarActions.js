8d30597caa83fdd8238993d4db82c49fce74f932
Chribtoper <xcstolfax@gmail.com>
2022-02-26 22:55:16 -0700

    Added comments for snackbarActions

--- src/actions/snackbarActions.js
+++ src/actions/snackbarActions.js
@@ -1,6 +1,18 @@
 import Button from "@mui/material/Button";
 import { snackbarTypes } from "../types/snackbarTypes";
 
+/**
+ * Callback used by setSnackbarError and setSnackbarSuccess.
+ * @callback dispatch
+ * @param {string} type
+ * @param {object} payload
+ */
+
+/**
+ * @param {error} error - An error object that has a message received from an api response.
+ * @param {dispatch} dispatch - A callback dispatch used to pass the return value of closeSnackbar to the uiActions reducer.
+ * @returns {dispatch} - The dispatch callback taking in the enqueue snackbarFunction.
+ */
 const setSnackbarError = (error, dispatch) => {
   return dispatch(
     enqueueSnackbar({
@@ -20,6 +32,11 @@ const setSnackbarError = (error, dispatch) => {
   );
 };
 
+/**
+ * @param {object} data - A data object that has a message received from an api response.
+ * @param {dispatch} dispatch - A callback dispatch used to pass the return value of closeSnackbar to the uiActions reducer.
+ * @returns {dispatch} - The dispatch callback taking in the enqueue snackbarFunction.
+ */
 const setSnackbarSuccess = (data, dispatch) => {
   return dispatch(
     enqueueSnackbar({
@@ -39,6 +56,11 @@ const setSnackbarSuccess = (data, dispatch) => {
   );
 };
 
+/**
+ * This is what triggers the error and success messages that pop up on the bottom left of a users screen.
+ * @param {object} data - An object containing notification data object with an options object containing a random key, a success or error variant and an action button.
+ * @returns {object} - An object containing the type of snackbar, payload data and the key associated with it.
+ */
 const enqueueSnackbar = (data) => {
   const key = data.notification.options && data.notification.options.key;
   return {
@@ -50,6 +72,11 @@ const enqueueSnackbar = (data) => {
   };
 };
 
+/**
+ * This function triggers closing a snackbar that has popped up on a users screen.
+ * @param {number} key - A key associated with the snackbar message so the reducer knows which message to remove.
+ * @returns {object} - An object containing the snackbar type and a payload containing the key as well as a dismissAll boolean.
+ */
 const closeSnackbar = (key) => ({
   type: snackbarTypes.CLOSE_SNACKBAR,
   payload: {
@@ -58,6 +85,11 @@ const closeSnackbar = (key) => ({
   },
 });
 
+/**
+ * This function triggers when a certain amount of time has passed or a user has triggered closeSnackbar
+ * @param {number} key - A key associated with the snackbar message so the reducer knows which message to remove.
+ * @returns {object} - Containing a type and a payload so that the reducer knows what to remove.
+ */
 const removeSnackbar = (key) => {
   return { type: snackbarTypes.REMOVE_SNACKBAR, payload: { key: key } };
 };
