66344a98a92d7b4dc1737173e38fdc0621890a3f
Chribtoper <xcstolfax@gmail.com>
2022-02-28 13:57:37 -0700

    Added api/ before each controller

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ export const LOCAL_STORAGE_KEY = "user-data";
 
 // API controller names
 export const controllers = {
-  users: "users",
-  books: "books",
-  quotes: "quotes",
+  users: "api/users",
+  books: "api/books",
+  quotes: "api/quotes",
 };
 
 // API endpoints
