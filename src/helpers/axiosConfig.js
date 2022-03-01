b14b9b6b5cc27770e7d014b5aa5a512fb7cf5b21
Chribtoper <xcstolfax@gmail.com>
2022-03-01 00:06:21 -0700

    Maybe moving defaults to the top will change something

--- src/helpers/axiosConfig.js
+++ src/helpers/axiosConfig.js
@@ -1,12 +1,9 @@
-// First we need to import axios.js
 import Axios from "axios";
-// Next we make an 'instance' of it
+Axios.defaults.withCredentials = true;
+
 const axios = Axios.create({
-  // .. where we make our configurations
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
 });
-
-Axios.defaults.withCredentials = true;
 
 export default axios;
