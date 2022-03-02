7e65495024bed3147211682bf0aa238c63f68340
Chribtoper <xcstolfax@gmail.com>
2022-03-02 14:12:12 -0700

    Updated error page, adding styling

--- src/pages/Error404Page/Error404Page.js
+++ src/pages/Error404Page/Error404Page.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { Container, Typography } from "@mui/material";
 
-const Error404Page = () => <h1>Error 404. Page does not exist</h1>;
+const Error404Page = () => (
+  <Container maxWidth="sm" sx={{ marginTop: 8, textAlign: "center" }}>
+    <Typography
+      component="h1"
+      variant="h3"
+      sx={{ textShadow: "1px 1px 2px gray" }}
+    >
+      Error 404 - Page does not exist
+    </Typography>
+  </Container>
+);
 
 export default Error404Page;
